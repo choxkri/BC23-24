357e0d50a12b25a21e753a79709934fbd2c3e19d
choxkri <1059788@hr.nl>
2023-09-05 13:33:45 +0200

    finished w1

--- arch 1/w1/A1W1A2 - Tax & Tip.py
+++ arch 1/w1/A1W1A2 - Tax & Tip.py
@@ -1,4 +1,29 @@
-cost = float(input("Cost of the meal: "))
-tax = round(cost * 0.21, 3)
-tip = round(cost * 0.15, 3)
-print(f"Tax: {tax}, Tip: {tip}, Total: {round(cost + tax + tip, 3)}")
+'''
+Assignment description
+The program that you create for this exercise will begin by reading the cost of a meal ordered at
+a restaurant from the user.
+Then your program will compute the tax and tip for the meal. Use your local tax rate when
+computing the amount of tax owing.
+The output from your program should include the tax amount, the tip amount, and the grand total
+for the meal including both the tax and the tip.
+Criteria:
+Tip is 15 percent of meal amount (without the tax)
+Assume a local tax rate of 21 percent
+Round all numbers up to 3 decimals in the output
+Input example:
+Cost of the meal: 23.60
+Output example:
+Tax: 4.956 , Tip: 3.540 , Total: 32.096
+'''
+
+meal_cost = float(input("Cost of the meal: "))
+
+tax = (meal_cost * 0.21)
+tip = (meal_cost * 0.15)
+total = (meal_cost + tax + tip)
+
+tax = '{:.3f}'.format(tax)
+tip = '{:.3f}'.format(tip)
+total = '{:.3f}'.format(total)
+
+print(f"Tax: {tax}, Tip: {tip}, Total: {total}")
