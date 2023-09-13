e964693a9d9b5f6b5a604d3f2a02d2da772ba508
choxkri <1059788@hr.nl>
2023-09-13 11:49:07 +0200

    finished truth tables

--- arch 1/w3/A1W3P7 - Truth tables.py
+++ arch 1/w3/A1W3P7 - Truth tables.py
@@ -1,2 +1,19 @@
-def urmom(name):
-    print(f"yo momma {name}")
+states = [(True, True), (True, False), (False, True), (False, False)]
+
+print("AND")
+print("---------------------")
+for state in states:
+    result = False
+    if state[0] and state [1]:
+        result = True
+    print(f"{state[0]} + {state[1]} = {result}")
+
+print()
+
+print("OR")
+print("---------------------")
+for state in states:
+    result = False
+    if state[0] or state [1]:
+        result = True
+    print(f"{state[0]} + {state[1]} = {result}")
