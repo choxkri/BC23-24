05b07ae282b5c1e60c316119cbffcb4e5faf3e59
choxkri <1059788@hr.nl>
2023-09-21 23:00:46 +0200

    finished taxi fares

--- arch 2/A2W5P2 - Taxi Fares.py
+++ arch 2/A2W5P2 - Taxi Fares.py
@@ -1,1 +1,12 @@
-base = 4
+import math
+base = 4.0
+fpd = 0.25
+
+def calculate_fare(distance: float):
+    kms = distance * 1000
+    fares = math.ceil(kms / 140)
+    return str(fpd * fares + base)
+
+user = float(input("Distance traveled: "))
+
+print(f"Total fare: {calculate_fare(user)} EUR")
