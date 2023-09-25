e6bb4d54ebf5394be0d6530d8b069455c94581ab
choxkri <1059788@hr.nl>
2023-09-26 00:21:07 +0200

    hamza fix

---
+++ peercoaching/hamza_code2.py
@@ -0,0 +1,31 @@
+x = ''
+
+ 
+
+while len(x) == 0:
+    x = str(input("String: "))
+    x1 = x.replace(",","")
+    x2 = x1.replace("!","")
+    x3 = x2.replace("?","")
+    x4 = x3.replace(".","")
+
+ 
+
+newlist = []
+for i in x:
+    newlist.append(i)
+
+
+ 
+
+newstring = ""
+newlist.reverse()
+for j in newlist:
+    newstring += j
+
+ 
+
+if x4.lower() == newstring.lower():
+    print(x4.lower() +" is a palindrome")
+else:
+    print(x4.lower() +" is not a palindrome")
