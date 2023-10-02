440b332c997f73f705ead851ab2fa95560fc3cee
choxkri <1059788@hr.nl>
2023-10-03 01:05:03 +0200

    started

---
+++ arch 2/w1/A2W5P6 - Twelve Days of Christmas.py
@@ -0,0 +1,28 @@
+d_to_l = {
+    1 : "1st",
+    2 : "2nd",
+    1 : "3rd",
+    1 : "4th",
+    1 : "5th",
+    1 : "6th",
+    1 : "7th",
+    1 : "8th",
+    1 : "9th",
+    1 : "10th",
+    1 : "11th",
+    1 : "12th"
+}
+
+verses = [
+    "Two turtle doves",
+    "Three French hens",
+    "Four calling birds",
+    "Five gold rings",
+    "Six geese a-laying",
+    "Seven swans a-swimming",
+    "Eight maids a-milking",
+    "Nine ladies dancing",
+    "Ten lords a-leaping",
+    "Eleven pipers piping",
+    "Twelve drummers drumming"
+]
