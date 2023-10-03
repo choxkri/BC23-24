c449c33cba9b0969df8f8ad19245104922f86d58
choxkri <1059788@hr.nl>
2023-10-03 13:02:38 +0200

    finished twelve days of christmas

--- arch 2/w1/A2W5P6 - Twelve Days of Christmas.py
+++ arch 2/w1/A2W5P6 - Twelve Days of Christmas.py
@@ -1,19 +1,20 @@
 d_to_l = {
     1 : "1st",
     2 : "2nd",
-    1 : "3rd",
-    1 : "4th",
-    1 : "5th",
-    1 : "6th",
-    1 : "7th",
-    1 : "8th",
-    1 : "9th",
-    1 : "10th",
-    1 : "11th",
-    1 : "12th"
+    3 : "3rd",
+    4 : "4th",
+    5 : "5th",
+    6 : "6th",
+    7 : "7th",
+    8 : "8th",
+    9 : "9th",
+    10 : "10th",
+    11 : "11th",
+    12 : "12th"
 }
 
 verses = [
+    "A partridge in a pear tree",
     "Two turtle doves",
     "Three French hens",
     "Four calling birds",
@@ -26,3 +27,24 @@ verses = [
     "Eleven pipers piping",
     "Twelve drummers drumming"
 ]
+
+joinlist = [
+    "Two turtle doves And A partridge in a pear tree"
+]
+
+def next_verse(vers_number: int) -> str:
+    returnstring = f"On the {d_to_l[vers_number]} day of Christmas, my true love sent to me "
+    if vers_number == 1:
+        return returnstring + verses[0]
+    
+    elif vers_number == 2:
+        returnstring = f"On the {d_to_l[vers_number]} day of Christmas, my true love sent to me "
+        return returnstring + joinlist[0]
+    
+    else:
+        returnstring = f"On the {d_to_l[vers_number]} day of Christmas, my true love sent to me "
+        joinlist.insert(0, verses[vers_number - 1])
+        return returnstring + ", ".join(joinlist)
+
+for i in range(1, 13):
+    print(next_verse(i))
