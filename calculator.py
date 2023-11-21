ba3f27cdcaa19037432727455a735dcd01e49af6
choxkri <1059788@hr.nl>
2023-11-21 21:32:46 +0100

    eee

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def summation(num1: int, num2: int) -> int:
+    
+    return num1 + num2
+
+def subtraction(num1: int, num2: int) -> int:
+    
+    return num1 - num2
+
+def multiplication(num1: int, num2: int) -> int:
+    
+    return num1 - num2
+
+def division(num1: int, num2: int) -> float:
+    """
+    Calculate the division of two number
+    """
+    return num1 / num2
+
